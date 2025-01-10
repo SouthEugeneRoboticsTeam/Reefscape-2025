9352b9a41d2057eaa63328bf8b838176542a1702
Drive 1/Theodore <benji.urbancic@gmail.com>
2025-01-09 17:07:07 -0800

    more setup

--- src/test/java/org/sert2521/reefscape2025/ExampleTestSubsystem.kt
+++ src/test/java/org/sert2521/reefscape2025/ExampleTestSubsystem.kt
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test
 
 import org.junit.jupiter.api.Assertions.*
 
-internal class ExampleTest
+internal class ExampleTestSubsystem
 {
     // To learn more about how to write unit tests, see the
     // JUnit 5 User Guide at https://junit.org/junit5/docs/current/user-guide/
