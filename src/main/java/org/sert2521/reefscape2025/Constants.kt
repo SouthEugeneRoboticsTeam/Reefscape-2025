015c30c20bb508907316850e16db43804b928daa
Kona <conradulysses@outlook.com>
2025-01-17 11:40:07 -0800

    Added some stuff to Constants.kt (づ｡◕‿‿◕｡)づ

--- src/main/java/org/sert2521/reefscape2025/Constants.kt
+++ src/main/java/org/sert2521/reefscape2025/Constants.kt
@@ -1,2 +1,21 @@
 package org.sert2521.reefscape2025
 
+object ConfigConstants {
+
+}
+
+object PhysicalConstants {
+
+}
+
+object ElectronicIDs {
+
+}
+
+object TunedConstants {
+
+}
+
+object RuntimeConstants {
+
+}
