739abb4db61648f219d18c64b78358d6de5ddb23
Ely May <elystanmay@gmail.com>
2025-01-18 13:58:58 -0800

    set up basic subsystem code. <(•ω•)>

--- src/main/java/org/sert2521/reefscape2025/subsystems/Dispenser.kt
+++ src/main/java/org/sert2521/reefscape2025/subsystems/Dispenser.kt
@@ -1,13 +1,32 @@
 package org.sert2521.reefscape2025.subsystems
 
+import com.revrobotics.spark.SparkBase
 import com.revrobotics.spark.SparkLowLevel
 import com.revrobotics.spark.SparkMax
+import com.revrobotics.spark.config.SparkBaseConfig
+import com.revrobotics.spark.config.SparkMaxConfig
 import edu.wpi.first.wpilibj.DigitalInput
 import org.sert2521.reefscape2025.ElectronicIDs
 
-//change subsystem name to something shorter
 
 object Dispenser {
     private val dispenserMotor = SparkMax(ElectronicIDs.DISPENSER_MOTOR_ID, SparkLowLevel.MotorType.kBrushless)
     private val beamBreakSensor = DigitalInput(ElectronicIDs.BEAMBREAK_ID)
+    private val config = SparkMaxConfig()
+
+    init{
+        config.inverted(false)
+        config.smartCurrentLimit(30)
+        config.idleMode(SparkBaseConfig.IdleMode.kBrake)
+        dispenserMotor.configure(config, SparkBase.ResetMode.kResetSafeParameters, SparkBase.PersistMode.kPersistParameters)
+    }
+    fun getBeamBreak():Boolean{
+        return !beamBreakSensor.get()
+    }
+    fun setSpeed(speed:Double){
+        dispenserMotor.set(speed)
+    }
+    fun stop(){
+        dispenserMotor.stopMotor()
+    }
 }
