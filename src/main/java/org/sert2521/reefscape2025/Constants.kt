23c8540732b403fe6d6c1ccdb2e9706612df40b1
Ely May <elystanmay@gmail.com>
2025-01-17 17:20:29 -0800

    Began work on the subsystem, defined motors and beambreak, with constants. <(•ω•)>

--- src/main/java/org/sert2521/reefscape2025/Constants.kt
+++ src/main/java/org/sert2521/reefscape2025/Constants.kt
@@ -1,2 +1,7 @@
 package org.sert2521.reefscape2025
 
+object ElectronicIDs {
+    const val CORAL_MANIPULATOR_MOTOR_ID = 0
+    const val BEAMBREAK_ID = 0
+
+}
--- src/main/java/org/sert2521/reefscape2025/subsystems/CoralManipulator.kt
+++ src/main/java/org/sert2521/reefscape2025/subsystems/CoralManipulator.kt
@@ -1,4 +1,13 @@
 package org.sert2521.reefscape2025.subsystems
 
+import com.revrobotics.spark.SparkLowLevel
+import com.revrobotics.spark.SparkMax
+import edu.wpi.first.wpilibj.DigitalInput
+import org.sert2521.reefscape2025.ElectronicIDs
+
+//change subsystem name to something shorter
+
 object CoralManipulator {
+    private val coralManipulatorMotor = SparkMax(ElectronicIDs.CORAL_MANIPULATOR_MOTOR_ID, SparkLowLevel.MotorType.kBrushless)
+    private val beamBreakSensor = DigitalInput(ElectronicIDs.BEAMBREAK_ID)
 }
