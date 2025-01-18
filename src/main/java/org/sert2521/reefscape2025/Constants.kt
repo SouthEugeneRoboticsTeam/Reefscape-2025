8d50a0dc1c666ba685117e81cc555f227d299253
Ely May <elystanmay@gmail.com>
2025-01-17 17:34:16 -0800

    renamed variables to use "dispenser" instead of "coral manipulator". <(•ω•)>

--- src/main/java/org/sert2521/reefscape2025/Constants.kt
+++ src/main/java/org/sert2521/reefscape2025/Constants.kt
@@ -1,7 +1,7 @@
 package org.sert2521.reefscape2025
 
 object ElectronicIDs {
-    const val CORAL_MANIPULATOR_MOTOR_ID = 0
+    const val DISPENSER_MOTOR_ID = 0
     const val BEAMBREAK_ID = 0
 
 }
--- src/main/java/org/sert2521/reefscape2025/subsystems/Dispenser.kt
+++ src/main/java/org/sert2521/reefscape2025/subsystems/Dispenser.kt
@@ -7,7 +7,7 @@ import org.sert2521.reefscape2025.ElectronicIDs
 
 //change subsystem name to something shorter
 
-object CoralManipulator {
-    private val coralManipulatorMotor = SparkMax(ElectronicIDs.CORAL_MANIPULATOR_MOTOR_ID, SparkLowLevel.MotorType.kBrushless)
+object Dispenser {
+    private val dispenserMotor = SparkMax(ElectronicIDs.DISPENSER_MOTOR_ID, SparkLowLevel.MotorType.kBrushless)
     private val beamBreakSensor = DigitalInput(ElectronicIDs.BEAMBREAK_ID)
 }
