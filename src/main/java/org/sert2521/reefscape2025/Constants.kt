9651d9650651c6c112255edd71705f63ea36c90c
Kona <conradulysses@outlook.com>
2025-01-19 15:09:47 -0800

    Added Ground Intake subsystem to Wrist branch, also re-added commands folder because for some reason it was deleted (づ｡◕‿‿◕｡)づ

--- src/main/java/org/sert2521/reefscape2025/Constants.kt
+++ src/main/java/org/sert2521/reefscape2025/Constants.kt
@@ -2,6 +2,8 @@ package org.sert2521.reefscape2025
 
 
 object ElectronicIDs {
+
     const val WRIST_MOTOR_ID = -1
+    const val INTAKE_ROLLER_ID = -1
 
 }
---
+++ src/main/java/org/sert2521/reefscape2025/subsystems/GroundIntake.kt
@@ -0,0 +1,27 @@
+package org.sert2521.reefscape2025.subsystems
+
+import com.revrobotics.spark.SparkLowLevel
+import com.revrobotics.spark.SparkMax
+import com.revrobotics.spark.config.SparkBaseConfig
+import com.revrobotics.spark.config.SparkMaxConfig
+import edu.wpi.first.wpilibj2.command.Subsystem
+import org.sert2521.reefscape2025.ElectronicIDs
+
+object GroundIntake: Subsystem {
+
+    val rollerMotor = SparkMax(ElectronicIDs.INTAKE_ROLLER_ID, SparkLowLevel.MotorType.kBrushless)
+    private val config = SparkMaxConfig()
+
+    init {
+
+        config.idleMode(SparkBaseConfig.IdleMode.kBrake)
+        config.smartCurrentLimit(30)
+        config.inverted(false)
+
+    }
+
+    fun setSpeed(speed: Double) { rollerMotor.set(speed) }
+
+    fun stop() { rollerMotor.stopMotor() }
+
+}
