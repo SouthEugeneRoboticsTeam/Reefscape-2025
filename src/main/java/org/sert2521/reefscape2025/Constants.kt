b9304820577cb0c5e51fb5bb6ff83ae53d89e13e
Ely May <elystanmay@gmail.com>
2025-01-19 15:47:27 -0800

    created wrist functions. <(•ω•)>

--- src/main/java/org/sert2521/reefscape2025/Constants.kt
+++ src/main/java/org/sert2521/reefscape2025/Constants.kt
@@ -5,5 +5,9 @@ object ElectronicIDs {
 
     const val WRIST_MOTOR_ID = -1
     const val INTAKE_ROLLER_ID = -1
-
+    const val WRIST_ABSOLUTE_ENCODER = -1
+}
+object PhysicalConstants {
+    const val WRIST_ENCODER_MULTIPLIER = 1.0
+    const val WRIST_ENCODER_TRANSFORM = 0.0
 }
--- src/main/java/org/sert2521/reefscape2025/subsystems/Wrist.kt
+++ src/main/java/org/sert2521/reefscape2025/subsystems/Wrist.kt
@@ -5,13 +5,16 @@ import com.revrobotics.spark.SparkLowLevel
 import com.revrobotics.spark.SparkMax
 import com.revrobotics.spark.config.SparkBaseConfig
 import com.revrobotics.spark.config.SparkMaxConfig
+import edu.wpi.first.wpilibj.DutyCycleEncoder
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import org.sert2521.reefscape2025.ElectronicIDs
+import org.sert2521.reefscape2025.PhysicalConstants
+import kotlin.math.PI
 
 object Wrist : SubsystemBase() {
     private val wristMotor = SparkMax(ElectronicIDs.WRIST_MOTOR_ID, SparkLowLevel.MotorType.kBrushless)
     private val config = SparkMaxConfig()
-    //absolute encoder
+    private val absoluteEncoder = DutyCycleEncoder(ElectronicIDs.WRIST_ABSOLUTE_ENCODER)
 
     init {
         config.inverted(false)
@@ -19,11 +22,21 @@ object Wrist : SubsystemBase() {
         config.idleMode(SparkBaseConfig.IdleMode.kBrake)
         wristMotor.configure(config, SparkBase.ResetMode.kResetSafeParameters, SparkBase.PersistMode.kPersistParameters)
     }
-
-    fun setheight(height:Double) {
-
+    fun getEncoder():Double {
+        return absoluteEncoder.get()
     }
+    fun getAngle(): Double {
+        var wristAngle = (absoluteEncoder.get())* PhysicalConstants.WRIST_ENCODER_MULTIPLIER + PhysicalConstants.WRIST_ENCODER_TRANSFORM
 
+        if (wristAngle<-PI) {
+            wristAngle += 2 * PI
+        }
+
+        return wristAngle
+    }
+    fun setSpeed(speed:Double) {
+        wristMotor.set(speed)
+    }
     fun stop(){
         wristMotor.stopMotor()
     }
