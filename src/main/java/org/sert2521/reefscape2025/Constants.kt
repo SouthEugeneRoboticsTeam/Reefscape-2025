f1c62bba810e0b6c064e625a72ea1413719b1a13
Kona <conradulysses@outlook.com>
2025-01-21 10:46:04 -0800

    Fixed a couple of drivetrain related constant issues ʕ•́ᴥ•̀ʔっ

--- src/main/java/org/sert2521/reefscape2025/Constants.kt
+++ src/main/java/org/sert2521/reefscape2025/Constants.kt
@@ -6,8 +6,12 @@ import edu.wpi.first.math.Nat
 import edu.wpi.first.math.geometry.Translation2d
 import edu.wpi.first.math.numbers.N1
 import edu.wpi.first.math.numbers.N3
+import org.sert2521.reefscape2025.PhysicalConstants.HALF_SIDE_LENGTH
+import kotlin.math.PI
 
 object ConfigConstants {
+
+    // Drivetrain speeds and stuff are in DrivetrainConstants
 
 }
 
@@ -16,6 +20,9 @@ object PhysicalConstants {
     const val WRIST_ENCODER_OFFSET = 0.0
     const val WRIST_ENCODER_MULTIPLIER = 0.0
     const val WRIST_ENCODER_TRANSFORM = 0.0
+
+    const val HALF_SIDE_LENGTH = 0.0
+    const val DRIVE_BASE_RADIUS = 0.0
 
 }
 
@@ -76,7 +83,11 @@ object DrivetrainConstants {
 
     const val TURN_SPEED = 0.0
 
-    var swerveModuleData = listOf()
+    val swerveModuleData = listOf(
+        SwerveModuleData(Translation2d(HALF_SIDE_LENGTH, -HALF_SIDE_LENGTH), 5, 7, 16, -0.355-1.61-1.56+ PI /2, false), //Back Left
+        SwerveModuleData(Translation2d(-HALF_SIDE_LENGTH, -HALF_SIDE_LENGTH), 1, 2, 15, -0.138-1.57-1.54+ PI /2, false), //Back Right
+        SwerveModuleData(Translation2d(HALF_SIDE_LENGTH, HALF_SIDE_LENGTH), 16, 15, 14, 2.41-1.612-1.58+ PI /2, false), //Front Left
+        SwerveModuleData(Translation2d(-HALF_SIDE_LENGTH, HALF_SIDE_LENGTH), 3, 12, 13, 0.059-1.568-1.575+ PI /2, false)) //Front Right
 
     const val DRIVE_MOTOR_INVERTED = false
     const val ANGLE_MOTOR_INVERTED = false
