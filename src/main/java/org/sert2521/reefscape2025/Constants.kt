3d634a2441417e4298d7700c427e535f30ca4a29
Kona <conradulysses@outlook.com>
2025-01-20 16:49:03 -0800

    wheeeeee (づ｡◕‿‿◕｡)づ

--- src/main/java/org/sert2521/reefscape2025/Constants.kt
+++ src/main/java/org/sert2521/reefscape2025/Constants.kt
@@ -28,9 +28,9 @@ object TunedConstants {
     const val DISPENSER_OUTTAKE_SPEED = 0.0
 
     const val WRIST_CORAL_INTAKE_SPEED = 0.0
-    const val WRIST_CORAL_OUTTAKE_SPEED = 0.0 //Should be negative
+    const val WRIST_CORAL_OUTTAKE_SPEED = -0.0 //Should be negative
     const val WRIST_ALGAE_INTAKE_SPEED = 0.0
-    const val WRIST_ALGAE_OUTTAKE_SPEED = 0.0 //Should be negative
+    const val WRIST_ALGAE_OUTTAKE_SPEED = -0.0 //Should be negative
 }
 
 object CurrentLimits {
