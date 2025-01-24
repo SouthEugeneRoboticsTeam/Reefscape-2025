73e37ae8f2a16df0758cc1aae3bc18fb2940455b
Dylan Myers <dymyers@icloud.com>
2025-01-23 17:17:54 -0800

    quite possibly finished - setSpeed, setVoltage, getDistance, motors configured - TOF left default

--- src/main/java/org/sert2521/reefscape2025/subsystems/ElevatorSubsystem.kt
+++ src/main/java/org/sert2521/reefscape2025/subsystems/ElevatorSubsystem.kt
@@ -1,26 +1,61 @@
 package org.sert2521.reefscape2025.subsystems
 
-import com.ctre.phoenix6.StatusSignal
 import com.ctre.phoenix6.configs.CANrangeConfiguration
-import com.ctre.phoenix6.hardware.CANrange
 import com.ctre.phoenix6.hardware.core.CoreCANrange
+import com.revrobotics.spark.SparkBase
 import com.revrobotics.spark.SparkLowLevel
 import com.revrobotics.spark.SparkMax
-import edu.wpi.first.units.measure.Distance
+import com.revrobotics.spark.config.SparkBaseConfig
+import com.revrobotics.spark.config.SparkMaxConfig
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import org.sert2521.reefscape2025.ElectronicIDs
 
 object ElevatorSubsystem : SubsystemBase() {
     private val motorOne = SparkMax(ElectronicIDs.elevatorMotorOne, SparkLowLevel.MotorType.kBrushless)
     private val motorTwo = SparkMax(ElectronicIDs.elevatorMotorTwo, SparkLowLevel.MotorType.kBrushless)
-    private val CANrange = CoreCANrange(ElectronicIDs.TOFSensorID, "0" ) // TODO("set CAN ID)
-    private val CANrangeConfiguration = CANrangeConfiguration()
+    private val motorOneConfig = SparkMaxConfig()
+    private val motorTwoConfig = SparkMaxConfig()
+    private val CANrange = CoreCANrange(ElectronicIDs.TOFSensorID, "0") // TODO("set CAN ID)
+    private val CANrangeConfiguration: CANrangeConfiguration = CANrangeConfiguration()
+
+    init {
+
+        motorTwoConfig.inverted(false)
+        motorTwoConfig.smartCurrentLimit(40)
+        motorTwoConfig.idleMode(SparkBaseConfig.IdleMode.kBrake)
+
+        motorOneConfig.inverted(false)
+        motorOneConfig.smartCurrentLimit(40)
+        motorOneConfig.idleMode(SparkBaseConfig.IdleMode.kBrake)
+
+        motorOne.configure(
+            motorOneConfig,
+            SparkBase.ResetMode.kResetSafeParameters,
+            SparkBase.PersistMode.kPersistParameters
+        )
+        motorTwo.configure(
+            motorTwoConfig,
+            SparkBase.ResetMode.kResetSafeParameters,
+            SparkBase.PersistMode.kPersistParameters
+        )
+    }
+
 
     fun setVoltage(voltage: Double) {
         motorOne.setVoltage(voltage)
         motorTwo.setVoltage(voltage)
     }
-    fun getDistance() {
 
+    fun setSpeed(speed: Double) {
+        motorOne.set(speed)
+        motorTwo.set(speed)
+    }
+
+    fun getDistance(): Double {
+        CANrange.distance.refresh()
+        val distanceObject = CANrange.distance.value
+        val units = distanceObject.unit() // not sure what the units are by default - probably meters
+
+        return distanceObject.`in`(units)
     }
 }
